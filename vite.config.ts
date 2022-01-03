3a77bb0472dcc516bd7db01915fa0e12a5ca0b93
Join.G <joinhack@qq.com>
2022-01-03 08:40:05 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 export default defineConfig({
+    plugins: [viteSingleFile()],
     root: __dirname,
     base: './',
+
+    build: {
+        rollupOptions: {
+			inlineDynamicImports: true,
+			output: {
+				manualChunks: () => "svgpath.js",
+			},
+		}
+    },
     
     server: {
         port: 8080,
